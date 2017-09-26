1ca7b66d8727d6fe4feadb30870d9bc45f291bb9
Casper Hemmje Østergaard <cstergaard@hotmail.com>
2017-09-26 11:16:27 +0200

    Tilføjet test

---
+++ CalculatorTest/CalculatorTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SoapProviderCalculator;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        private ServiceCalculator calculator;
+
+        [TestInitialize]
+        public void SetUpCalculator()
+        {
+            calculator = new ServiceCalculator();
+        }
+
+        [TestMethod]
+        public void AddMethodTest()
+        {
+            Assert.AreEqual(10, calculator.Add(5, 5));
+        }
+    }
+}
